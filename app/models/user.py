741c0c2a46782a9eddadd7504ed3dde620c01bdc
Lisa Moshiro <lmmoshiro2000@gmail.com>
2019-10-12 20:12:44 -0400

    added_fields_to_user_class

--- app/models/user.py
+++ app/models/user.py
@@ -21,7 +21,7 @@ class Role(db.Model):
     default = db.Column(db.Boolean, default=False, index=True)
     permissions = db.Column(db.Integer)
     users = db.relationship('User', backref='role', lazy='dynamic')
-    
+
     @staticmethod
     def insert_roles():
         roles = {
@@ -60,6 +60,11 @@ class User(UserMixin, db.Model):
     email = db.Column(db.String(64), unique=True, index=True)
     password_hash = db.Column(db.String(128))
     role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+    phone_number = db.Column(db.String(16))
+    street = db.Column(db.String(64))
+    city = db.Column(db.String(64))
+    state = db.Column(db.String(2))
+    organization_corporation = db.Column(db.String(64))
 
     def __init__(self, **kwargs):
         super(User, self).__init__(**kwargs)
