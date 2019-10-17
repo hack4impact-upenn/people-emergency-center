1b3913769ba8671a961625b983bffabe3b2f766c
nabeelf <nfarooqui99@gmail.com>
2019-10-16 22:37:45 -0400

    finished staff view

--- app/staff/views.py
+++ app/staff/views.py
@@ -12,7 +12,7 @@ from flask_rq import get_queue
 
 from app import db
 from app.decorators import staff_required
-from app.models import EditableHTML, Role, User, Volunteer
+from app.models import EditableHTML, Role, User, Volunteer, Status
 
 staff = Blueprint('staff', __name__)
 
@@ -23,10 +23,17 @@ def index():
     """Volunteer dashboard page."""
     return render_template('staff/index.html')
 
-@staff.route('/view_volunteers')
+@staff.route('/view_volunteers', methods=['GET', 'POST'])
 @login_required
 @staff_required
 def view_volunteers():
     """View all volunteers."""
-    volunteers = Volunteer.query.all()
+    volunteers = Volunteer.query.filter(Volunteer.status1 == Status.CLEARED, Volunteer.status2 == Status.CLEARED, Volunteer.status3 == Status.CLEARED)
     return render_template('staff/view_volunteers.html', volunteers=volunteers)
+
+@staff.route('/view_one/<int:id>', methods=['GET'])
+@login_required
+@staff_required
+def view_one(id):
+    volunteer = Volunteer.query.get(id)
+    return render_template('staff/view_one.html', volunteer=volunteer)
