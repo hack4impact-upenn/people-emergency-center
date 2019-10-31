73897caba84f4798592134ce9f5dbd061f9bddec
Dzung Nguyen <dzungng@seas.upenn.edu>
2019-10-30 21:30:58 -0400

    added upload clearanceS

--- app/volunteer/views.py
+++ app/volunteer/views.py
@@ -34,12 +34,14 @@ def upload_clearances():
             current_volunteer.status1 = Status.SUBMITTED
         else:
             current_volunteer.status1 = Status.NOT_SUBMITTED
+
     elif form.validate_on_submit() and form.submit2.data:
         if form.link2.data.strip() != '':
             current_volunteer.link2 = form.link2.data
             current_volunteer.status2 = Status.SUBMITTED
         else:
             current_volunteer.status2 = Status.NOT_SUBMITTED
+
     elif form.validate_on_submit() and form.submit3.data:
         if form.link3.data.strip() != '':
             current_volunteer.link3 = form.link3.data
