7fb993e790ed461f3135be706aa75db5b7cc3bed
Dzung Nguyen <dzungng@seas.upenn.edu>
2020-01-08 16:15:59 +0700

    fixed sidebar nav menu - different for admin vs. volunteer

--- app/account/views.py
+++ app/account/views.py
@@ -386,3 +386,5 @@ def unconfirmed():
     if current_user.is_anonymous or current_user.confirmed:
         return redirect(url_for('main.index'))
     return render_template('account/unconfirmed.html')
+
+
