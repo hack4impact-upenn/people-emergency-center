da5fd848d3f1b2881ab9e5ea59ce8c33146c1d65
Dzung Nguyen <dzungng@seas.upenn.edu>
2020-01-13 14:45:08 +0700

    Fixed staff view_volunteers show volunteers with status3 not needed

--- app/staff/views.py
+++ app/staff/views.py
@@ -16,6 +16,7 @@ from app.models import EditableHTML, Role, User, Volunteer, Status
 
 staff = Blueprint('staff', __name__)
 
+
 @staff.route('/')
 @login_required
 @staff_required
@@ -23,13 +24,18 @@ def index():
     """Volunteer dashboard page."""
     return render_template('staff/index.html')
 
+
 @staff.route('/view_volunteers', methods=['GET', 'POST'])
 @login_required
 @staff_required
 def view_volunteers():
     """View all volunteers."""
-    volunteers = Volunteer.query.filter(Volunteer.status1 == Status.CLEARED, Volunteer.status2 == Status.CLEARED, Volunteer.status3 == Status.CLEARED)
+    volunteers = Volunteer.query.filter(Volunteer.status1 == Status.CLEARED,
+                                        Volunteer.status2 == Status.CLEARED,
+                                        Volunteer.status3 == Status.CLEARED or Status.NOT_NEEDED,
+                                        Volunteer.status4 == Status.CLEARED,)
     return render_template('staff/view_volunteers.html', volunteers=volunteers)
+
 
 @staff.route('/view_one/<int:id>', methods=['GET'])
 @login_required
